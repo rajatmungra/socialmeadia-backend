db0f02bbec034e787c2123956f4f205ffce90454
rajatmungra <rajat34780@gmail.com>
2023-07-17 21:49:29 +0530

    final

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
+
 /* READ */
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
